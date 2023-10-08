22641ce05fe4ed015fccd2cb13c8797aa6a2e19e
ajones119 <agj1113@gmail.com>
2023-10-08 09:42:21 -0500

    test

--- src/views/CampaignViews/Campaigns.tsx
+++ src/views/CampaignViews/Campaigns.tsx
@@ -33,7 +33,7 @@ export const Campaigns = () => {
       <Grid container justifyContent="center" alignItems="center">
         <Grid item xs={12}>
           <div className={css.addButton} style={{width: `${cardWidth-24}px`}}>
-            <Button size="large" color="dark" onClick={() => setIsCreateModalOpen(true)}>Add Campaignssssss</Button>
+            <Button size="large" color="dark" onClick={() => setIsCreateModalOpen(true)}>Add Campaigns</Button>
           </div>
         </Grid>
       </Grid>
