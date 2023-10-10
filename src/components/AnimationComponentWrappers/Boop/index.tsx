cd6c3a97cb3d4358437db12b0cf05858769fc861
ajones119 <agj1113@gmail.com>
2023-10-09 21:26:09 -0500

    Some animations added for buttons and copy buttons

---
+++ src/components/AnimationComponentWrappers/Boop/index.tsx
@@ -0,0 +1,14 @@
+import React, { PropsWithChildren } from 'react';
+import { animated } from '@react-spring/web';
+import useBoop, { BoopProps } from '../../../hooks/AnimationHooks/useBoop';
+
+const Boop = (props: PropsWithChildren<BoopProps>) => {
+  const {style, trigger} = useBoop(props);
+  return (
+    <animated.div onMouseEnter={trigger as any} style={{...style}}>
+      {props.children}
+    </animated.div>
+  );
+};
+
+export default Boop;
