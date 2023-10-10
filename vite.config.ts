705ab5b12ceeaca03e011dcd1e2b25d0ae15c5a8
ajones119 <agj1113@gmail.com>
2023-10-10 15:42:20 -0500

    Fixing Hot reloading on wsl

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
         open: true,
         // this sets a default port to 3000  
         port: 3000,
+        watch: {
+            usePolling: true
+        }
     },
     build: {
         rollupOptions: {
