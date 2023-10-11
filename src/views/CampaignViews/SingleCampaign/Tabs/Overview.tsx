f58c6ee6014ff2f66ddf9f6950d3f9e78147c185
ajones119 <agj1113@gmail.com>
2023-10-11 11:05:06 -0500

    adding dlipable cards

--- src/views/CampaignViews/SingleCampaign/Tabs/Overview.tsx
+++ src/views/CampaignViews/SingleCampaign/Tabs/Overview.tsx
@@ -17,14 +17,14 @@ const Overview = ({ campaign }: OverviewProps) => {
                     <Typography>{campaign.title}</Typography>
                 </Grid>
                 <Grid item xs={12}>
-                    <div className={css.overviewImage}>
+                    <div>
                         <img
+                            className={css.overviewImage}
                             src={campaign.campaignImageURL}
                             onError={({ currentTarget }) => {
                                 currentTarget.onerror = null; // prevents looping
                                 currentTarget.src=BACKUP;
                             }}
-                            width={900}
                             alt="boo"
                         />
                     </div>
