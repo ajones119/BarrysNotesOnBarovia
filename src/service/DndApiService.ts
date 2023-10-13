036c851cf36b2ad667c128e3a89ea9374fb79218
Colton Hurt <coltonhurt@live.com>
2023-10-12 22:12:31 -0500

    use drawer for adding monsters with search instead of input search in row

--- src/service/DndApiService.ts
+++ src/service/DndApiService.ts
@@ -1,4 +1,4 @@
-import { gql, useQuery } from '@apollo/client';
+import { gql, useQuery } from "@apollo/client";
 
 export const GET_MONSTERS = gql`
   query MonstersQuery($limit: Int!) {
@@ -20,25 +20,34 @@ type GQLMonsterResponse = {
   dexterity: number;
   armor_class: { value: number }[];
   hit_points: number;
-}
+};
 
-type Monster = Omit<GQLMonsterResponse, "index" | "armor_class" | "hit_points"> & {
+export type Monster = Omit<
+  GQLMonsterResponse,
+  "index" | "armor_class" | "hit_points"
+> & {
   id: string;
   armorClass: number;
   hitPoints: number;
-}
+};
 
-export const useDndApiMonsters = (): { monsters: Monster[], isLoading: boolean } => {
-  const { loading, error, data } = useQuery(GET_MONSTERS, { variables: { limit: 500 } });
+export const useDndApiMonsters = (): {
+  monsters: Monster[];
+  isLoading: boolean;
+} => {
+  const { loading, error, data } = useQuery(GET_MONSTERS, {
+    variables: { limit: 500 },
+  });
 
   return {
-    monsters: data?.monsters.map((monster: GQLMonsterResponse) => ({
-      id: monster.index,
-      armorClass: monster.armor_class[0].value,
-      hitPoints: monster.hit_points,
-      name: monster.name,
-      dexterity: monster.dexterity
-    })) ?? [],
-    isLoading: loading
+    monsters:
+      data?.monsters.map((monster: GQLMonsterResponse) => ({
+        id: monster.index,
+        armorClass: monster.armor_class[0].value,
+        hitPoints: monster.hit_points,
+        name: monster.name,
+        dexterity: monster.dexterity,
+      })) ?? [],
+    isLoading: loading,
   };
-}
+};
---
+++ src/views/DMInitiative/components/ResourceDrawer.tsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import { Monster, useDndApiMonsters } from "@services/DndApiService";
+import Drawer from "@components/Drawer";
+import { IconButton, ListItemText, TextField } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+
+type ResourceDrawerProps = {
+  onAdd?: (data: any) => void;
+};
+
+const ResourceDrawer = ({ onAdd }: ResourceDrawerProps) => {
+  const { isLoading, monsters } = useDndApiMonsters();
+
+  const [search, setSearch] = useState<string>();
+
+  if (isLoading || monsters.length === 0) return;
+
+  const handleMonsterAdd = (monster: Monster) => {
+    const monsterItem = {
+      name: monster.name,
+      health: monster.hitPoints,
+      maxHealth: monster.hitPoints,
+      armorClass: monster.armorClass,
+      initiativeBonus: Math.floor((monster.dexterity - 10) / 2),
+    };
+    onAdd?.(monsterItem);
+  };
+
+  return (
+    <Drawer>
+      <div style={{ padding: "16px" }}>
+        <TextField
+          id="monster-search"
+          variant="outlined"
+          label="Monster Search"
+          fullWidth
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            setSearch(event.target.value);
+          }}
+          value={search}
+        />
+        <Box>
+          <List>
+            {monsters
+              .filter(
+                (monster) =>
+                  !search || monster.name.toLowerCase().includes(search),
+              )
+              .map((monster) => (
+                <ListItem
+                  secondaryAction={
+                    <IconButton
+                      onClick={() => {
+                        handleMonsterAdd(monster);
+                      }}
+                      edge="end"
+                    >
+                      <AddIcon />
+                    </IconButton>
+                  }
+                >
+                  <ListItemText primary={monster.name} />
+                </ListItem>
+              ))}
+          </List>
+        </Box>
+      </div>
+    </Drawer>
+  );
+};
+
+export default ResourceDrawer;
