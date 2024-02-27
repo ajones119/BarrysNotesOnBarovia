154ba068066329040056c904ec846f1b6d10a387
ajones119 <agj1113@gmail.com>
2024-02-27 11:32:02 -0600

    Adding ai note summary

--- src/components/NotesList/NotesList.tsx
+++ src/components/NotesList/NotesList.tsx
@@ -3,12 +3,15 @@ import css from "./NotesList.module.scss";
 import { Note } from '@model/Note';
 import { Grid } from "@mui/material"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { faList, faNoteSticky, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 import { Button } from '../Button/Button';
 import CreateNoteModal from '../Modal/CreateNoteModal/CreateNoteModal';
 import { NotesListItem } from './NoteListItem';
 import { PlayerCharacter } from '@model/PlayerCharacter';
 import FloatingButtonContainer from '@components/FloatingButtonContainer';
+import GenerateSummaryNoteModal from '@components/Modal/GenerateSummaryNoteModal';
+import { format, isSameDay, parseISO } from 'date-fns';
+import { Typography } from '@components/Typography/Typography';
 const getCharacterFromNote = (note: Note, characters: PlayerCharacter[]) => {
     const notedCharacter = characters.find(character => note.characterDocId === character.docId)
 
@@ -21,29 +24,70 @@ declare interface NotesListProps {
     campaignId: string
 }
 
-const NotesList = ({characters, notes, campaignId}: NotesListProps) => {
+const NotesList = ({characters, notes = [], campaignId}: NotesListProps) => {
     const [isCreateModalOpen, setIsCreatemodalOpen] = useState(false);
+    const [isGenerateSummaryModalOpen, setIsGenerateSummaryModalOpen] = useState(false);
+
+    let notesByDate: any = {};
+
+    notes?.forEach((note: Note) => {
+        const dateString = format(note?.date || new Date(), "yyyy-MM-dd")
+
+        if (notesByDate[dateString] && notesByDate[dateString]?.length > 0) {
+            notesByDate[dateString].push(note)
+        } else {
+            notesByDate[dateString] = [note]
+        }
+    });
+
+    let seedNotes = notes.filter(note => note?.date && isSameDay(note?.date, new Date()));
+
+    console.log(seedNotes)
+
+    if (seedNotes.length < 1 && notes.length > 1) {
+        const mostRecentNote = notes?.toSorted(function (a, b) {
+            if ((a?.date || 0) < (b?.date || 0)) {
+              return -1;
+            }
+            if ((a?.date || 0) > (b?.date || 0)) {
+              return 1;
+            }
+            return 0;
+        });
+        seedNotes = [];
+        seedNotes.push(mostRecentNote[0])
+    }
 
     return (
         <>
         <FloatingButtonContainer>
-            <Button animatedHover size='large' color="secondary" onClick={() => setIsCreatemodalOpen(true)}>Add Note{" "}<FontAwesomeIcon icon={faPlusCircle} /></Button>
+            <Button animatedHover size='large' color="secondary" onClick={() => setIsCreatemodalOpen(true)}><Typography size="large">Add Note{" "}<FontAwesomeIcon icon={faPlusCircle} /></Typography></Button>
+            <Button animatedHover size='large' color="secondary" onClick={() => setIsGenerateSummaryModalOpen(true)}><Typography>Generate Recap{" "}<FontAwesomeIcon icon={faList} /></Typography></Button>
+
         </FloatingButtonContainer>
-        <Grid container justifyContent="space-around" alignItems="center">
-            <Grid item xs={12}>
-            
             <div className={css.notesList}>
                 {
-                    notes?.map((note: Note) => { 
-                        const character = getCharacterFromNote(note, characters);
-                        return(
-                        <NotesListItem characterImageURL={character?.characterImageURL} characterName={character?.name} note={note} />
-                    )})
+                    Object.keys(notesByDate).map(key => {
+                        return (
+                            <div>
+                                <div>
+                                    <Typography size="large">{format(parseISO(key), "MMM, dd, yyyy")}</Typography>
+                                </div>
+                                {
+                                    notesByDate[key]?.map((note: Note) => { 
+                                        const character = getCharacterFromNote(note, characters);
+                                        return(
+                                            <NotesListItem characterImageURL={character?.characterImageURL} characterName={character?.name} note={note} />
+                                        )})
+                                }
+                            </div>
+                        );
+                    })
                 }
             </div>
-            </Grid>
-        </Grid>
-        <CreateNoteModal characters={characters} campaignId={campaignId} isOpen={isCreateModalOpen} onClose={() => setIsCreatemodalOpen(false)} />
+        <CreateNoteModal seedNotes={seedNotes} characters={characters} campaignId={campaignId} isOpen={isCreateModalOpen} onClose={() => setIsCreatemodalOpen(false)} />
+        <GenerateSummaryNoteModal characters={characters} campaignId={campaignId} isOpen={isGenerateSummaryModalOpen} notes={notes.filter(note => !note?.isGeneratedSummary)} onClose={() => setIsGenerateSummaryModalOpen(false)} />
+
         </>
     );
 }
