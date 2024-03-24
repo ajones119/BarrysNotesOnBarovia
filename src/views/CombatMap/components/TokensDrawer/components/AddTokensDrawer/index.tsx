79c9978486bc5adc335bc05f0d80a8e830cbd1c9
ajones119 <agj1113@gmail.com>
2024-03-24 11:30:39 -0500

    save fav tokens + some map bugfixes

--- src/views/CombatMap/components/TokensDrawer/components/AddTokensDrawer/index.tsx
+++ src/views/CombatMap/components/TokensDrawer/components/AddTokensDrawer/index.tsx
@@ -2,9 +2,11 @@ import { Button } from "@components/Button/Button";
 import Drawer, { DrawerProps } from "@components/Drawer";
 import { Typography } from "@components/Typography/Typography";
 import INTERNAL_TOKENS, { InternalToken } from "@views/CombatMap/TokensConfig";
-import React, { useState } from "react"
+import React, { useMemo, useState } from "react"
 import css from "../../TokenDrawer.module.scss"
-import { faChevronUp, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faChevronUp, faPlus, faStar } from "@fortawesome/free-solid-svg-icons";
+import { faStar as emptyStar } from "@fortawesome/free-regular-svg-icons";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useDebounce } from "usehooks-ts";
 import { TextInput } from "@components/TextInput/TextInput";
@@ -12,6 +14,9 @@ import { Spacer } from "@components/Spacer/Spacer";
 import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
 import { useCustomTokens } from "@services/CustomTokensService";
 import { COLORS_MAP } from "@components/ColorPicker/ColorPicker";
+import useLocalCharacter from "@hooks/useLocalCharacter";
+import { useParams } from "react-router-dom";
+import { useCampaignCharacters, useEditPlayerCharacter } from "@services/CharacterService";
 
 declare interface AddTokensDrawerProps extends DrawerProps {
     onAddToken: (_newToken: InternalToken) => void
@@ -22,35 +27,78 @@ const AddTokenDrawer = ({
     onClose,
     onAddToken
 }: AddTokensDrawerProps) => {
-    const {tokens = [], isLoading: tokensLoading} = useCustomTokens();
-
+    const {tokens: remoteTokens = [], isLoading: isTokensLoading} = useCustomTokens();
+    const {CampaignId = ""} = useParams();
+    const {selectedCharacter} = useLocalCharacter(CampaignId)
+    const {characters, isLoading: isCharactersLoading} = useCampaignCharacters(CampaignId);
+    const {mutate, isLoading: isMutating} = useEditPlayerCharacter(() => {})
     const [search, setSearch] = useState("");
     const debouncedSearch = useDebounce(search, 250);
+    const character = characters.find(character => selectedCharacter === character.docId)
 
-    let categories = [ ...INTERNAL_TOKENS ];
+    const categories = useMemo(() => {
+        let categories = [ ...INTERNAL_TOKENS ];
+        if (remoteTokens) {
+            const customCategory = {
+                name: "Custom",
+                id: "custom",
+                tokens: remoteTokens?.map(token => ({
+                    ...token,
+                    color: token?.canChangeColor ? COLORS_MAP.Black : undefined,
+                    baseTokenId: token?.docId
+                }))
+            }
+            // @ts-ignore
+            categories.push(customCategory);
+        }
 
-    if (tokens) {
-        const customCategory = {
-            name: "Custom",
-            id: "custom",
-            tokens: tokens?.map(token => ({
-                ...token,
-                color: token?.canChangeColor ? COLORS_MAP.Black : undefined,
-                baseTokenId: token?.docId
+        if (character) {
+            let favTokenIds = character?.favoriteTokens || [];
+            const remappedInternalTokens: InternalToken[] = [];
+            categories.forEach(category => {
+                remappedInternalTokens.push(...category.tokens)
+            });
+
+
+            const favTokens: InternalToken[] = [];
+            favTokenIds.forEach(id => {
+                const token = remappedInternalTokens.find(token => id === token?.baseTokenId || id === token?.name)
+                token && favTokens.push(token)
+            })
+
+
+            const favoriteCategory = {
+                name: "Favorites",
+                id: "favorite",
+                tokens: favTokens
+            }
+            if (favoriteCategory.tokens.length > 0) {
+                // @ts-ignore
+                categories.unshift(favoriteCategory);
+            }
+        }
+
+        if (debouncedSearch) {
+            categories = categories.map(category => ({
+                ...category,
+                tokens: category?.tokens.filter(option => option.name.toLowerCase().includes(debouncedSearch)) || []
             }))
         }
-        // @ts-ignore
-        categories.push(customCategory);
-    }
+    
+        categories = categories.filter(category => category?.tokens?.length > 0) || []
 
-    if (debouncedSearch) {
-        categories = categories.map(category => ({
-            ...category,
-            tokens: category?.tokens.filter(option => option.name.toLowerCase().includes(debouncedSearch)) || []
-        }))
-    }
+        return categories;
+    }, [isMutating, selectedCharacter, isCharactersLoading, isTokensLoading, debouncedSearch])
 
-    categories = categories.filter(category => category?.tokens?.length > 0) || []
+    const handleAddFavorite = (id: string) => {
+        if (character && CampaignId) {
+            if (character?.favoriteTokens && character?.favoriteTokens?.includes(id)) {
+                mutate({docId: selectedCharacter, favoriteTokens: character?.favoriteTokens?.filter(token => id !== token)});
+            } else {
+                mutate({docId: selectedCharacter, favoriteTokens: (character?.favoriteTokens || [])?.concat(id)});
+            }
+        }
+    }
 
     return (
         <Drawer
@@ -80,21 +128,28 @@ const AddTokenDrawer = ({
                                         backgroundColor: "#0f1417"
                                     }}
                                 >
-                                    {category?.tokens.map(token => (
+                                    {category?.tokens.map(token => {
+                                        return (
                                         <div className={css.addTokenEntry} key={token.name}>
                                             <div className={css.addTokenInfo}>
                                                 <Typography>{token.name}</Typography>
                                             </div>
-                                            <Button color='dark' onClick={() => onAddToken(token)}>
-                                                <FontAwesomeIcon icon={faPlus} />
-                                            </Button>
+                                                <div className={css.buttons}>
+                                                    <Button color='dark' onClick={() => onAddToken(token)}>
+                                                        <FontAwesomeIcon icon={faPlus} />
+                                                    </Button>
+                                                    { character && 
+                                                        <Button color='tertiary' onClick={() => handleAddFavorite(token?.baseTokenId || token?.name)}>
+                                                            <FontAwesomeIcon icon={character?.favoriteTokens?.includes(token?.baseTokenId || token?.name) ? faStar : emptyStar} />
+                                                        </Button>
+                                                    }
+                                            </div>
                                         </div>
-                                    ))}
+                                    )})}
                                 </AccordionDetails>
                             </Accordion>
                         ))
                     }
-               
             </div>
         </Drawer>
     );
